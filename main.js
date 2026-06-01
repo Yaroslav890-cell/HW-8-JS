b05b9d1352db1558f0f204127189778bde575f3f
Shapoval Yaroslav <pamp.s11052013@gmail.com>
2026-06-01 13:58:49 +0300

    commit

--- main.js
+++ main.js
@@ -22,3 +22,41 @@ const friends2= ['Mango', 'Poly', 'Kiwi', 'Ajax'];
 
 let string2 = friends2.join(',');
 console.log(string2);
+
+
+
+
+
+
+
+const cards = [
+  'Карточка-1',
+  'Карточка-2',
+  'Карточка-3',
+  'Карточка-4',
+  'Карточка-5',
+];
+const cardToRemove = 'Карточка-3';
+
+const index = cards.indexOf(cardToRemove);
+
+cards.splice(index, 1);
+
+console.log(cards);
+
+const cardToInsert = 'Карточка-6';
+
+const index2 = cards.indexOf('Карточка-4');
+
+cards.splice(index2, 0, cardToInsert);
+
+console.log(cards);
+
+
+const cardToUpdate = 'Карточка-4';
+
+const index3 = cards.indexOf(cardToUpdate);
+
+cards.splice(index3, 1, 'Оновлена Карточка-4');
+
+console.log(cards);
